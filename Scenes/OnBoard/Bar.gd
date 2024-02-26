b14ed73d9f4fdffefcc2b5a5e299863b5d484910
fuchsst <fuchsst>
2024-02-26 22:19:56 +0100

    added DialogPlayer.gd

--- Scenes/OnBoard/Bar.gd
+++ Scenes/OnBoard/Bar.gd
@@ -4,6 +4,7 @@ extends CanvasLayer
 @onready var left_character: BarCharacter = $ForegroundCanvas/TableTop/LeftCharacter
 @onready var right_character: BarCharacter = $ForegroundCanvas/TableTop/RightCharacter
 @onready var table_bottom: Node2D = $ForegroundCanvas/TableBottom
+@onready var cursor: Cursor = $OverlayCanvas/Cursor
 
 
 # Called when the node enters the scene tree for the first time.
@@ -19,3 +20,22 @@ func _ready():
 	$ForegroundCanvas/TableBottom/OnePersonLeft.visible = left_character.active_character_node and not right_character.active_character_node
 	$ForegroundCanvas/TableBottom/TwoPerson.visible = left_character.active_character_node and right_character.active_character_node
 
+
+
+func _on_talk_to_shotglass_button_pressed() -> void:
+	var cutscene_player = preload("res://Scenes/OnBoard/DialogPlayer.tscn").instantiate()
+	cutscene_player.set_dialogs(World.current_mission_dialogs.bar_shotglass)
+	get_tree().root.add_child(cutscene_player)
+	get_tree().root.remove_child(self)
+
+
+func _on_view_pilot_stats_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/OnBoard/Scoreboard.tscn")
+
+
+func _on_talk_to_left_character_button_mouse_entered() -> void:
+	cursor._on_interaction_area_entered("Talk to " + left_character.get_active_character_name())
+
+
+func _on_talk_to_right_character_button_mouse_entered() -> void:
+	cursor._on_interaction_area_entered("Talk to " + right_character.get_active_character_name())
--- Scenes/OnBoard/DialogPlayer.gd
+++ Scenes/OnBoard/DialogPlayer.gd
@@ -1,74 +1,143 @@
 class_name DialogPlayer
 extends Node2D
 
-@export var mission_dialogs: MissionDialogs
+@export var mission_dialogs: Array[DialogProperties]
+@export var background: PackedScene
+@export var foreground: PackedScene
 @onready var subtitles_label: Label = $SubtitlesLabel
-@onready var cutscene_character: CutsceneCharacter = $CutsceneCharacter
+@onready var cutscene_character: CutsceneCharacter = $Foreground
 
-var phoneme_to_viseme_with_durations = {
-	'a': {'viseme': 1, 'duration':  200/600},
-	'i': {'viseme': 1, 'duration':  200/600},
-	'e': {'viseme': 2, 'duration':  200/600},
-	'y': {'viseme': 3, 'duration':  200/600},
-	'o': {'viseme': 4, 'duration':  200/600},
-	'w': {'viseme': 4, 'duration':  200/600},
-	'u': {'viseme': 5, 'duration':  200/600},
-	'f': {'viseme': 6, 'duration':  150/600},
-	'v': {'viseme': 6, 'duration':  150/600},
-	'l': {'viseme': 7, 'duration':  150/600},
-	'r': {'viseme': 7, 'duration':  150/600},
-	'n': {'viseme': 7, 'duration':  150/600},
-	'm': {'viseme': 8, 'duration':  150/600},
-	'b': {'viseme': 8, 'duration':  100/600},
-	'p': {'viseme': 8, 'duration':  100/600},
-	'q': {'viseme': 9, 'duration':  150/600},
-	'g': {'viseme': 9, 'duration':  100/600},
-	'k': {'viseme': 9, 'duration':  100/600},
-	'd': {'viseme': 9, 'duration':  100/600},
-	'c': {'viseme': 9, 'duration':  150/600},
-	's': {'viseme': 10, 'duration': 150/600},
-	'z': {'viseme': 10, 'duration': 150/600},
-	't': {'viseme': 10, 'duration': 100/600},
-	'h': {'viseme': 10, 'duration': 150/600}
+
+
+const phoneme_to_viseme_with_durations = {
+	'a': {'viseme': 1, 'duration':  0.20},
+	'i': {'viseme': 1, 'duration':  0.20},
+	'e': {'viseme': 2, 'duration':  0.20},
+	'y': {'viseme': 3, 'duration':  0.20},
+	'o': {'viseme': 4, 'duration':  0.20},
+	'w': {'viseme': 4, 'duration':  0.20},
+	'u': {'viseme': 5, 'duration':  0.20},
+	'f': {'viseme': 6, 'duration':  0.15},
+	'v': {'viseme': 6, 'duration':  0.15},
+	'l': {'viseme': 7, 'duration':  0.15},
+	'r': {'viseme': 7, 'duration':  0.15},
+	'n': {'viseme': 7, 'duration':  0.15},
+	'm': {'viseme': 8, 'duration':  0.15},
+	'b': {'viseme': 8, 'duration':  0.10},
+	'p': {'viseme': 8, 'duration':  0.10},
+	'q': {'viseme': 9, 'duration':  0.15},
+	'g': {'viseme': 9, 'duration':  0.10},
+	'k': {'viseme': 9, 'duration':  0.10},
+	'd': {'viseme': 9, 'duration':  0.10},
+	'c': {'viseme': 9, 'duration':  0.15},
+	's': {'viseme': 10, 'duration': 0.15},
+	'z': {'viseme': 10, 'duration': 0.15},
+	't': {'viseme': 10, 'duration': 0.10},
+	'h': {'viseme': 10, 'duration': 0.15}
 }
 
+const subtitle_colors = [
+  Color(0.678, 0.847, 0.902),  # 0: Light blue
+  Color(0.702, 0.533, 0.870),  # 1: Lavendar (Spirit)
+  Color(0.255, 0.412, 0.882),  # 2: Royal Blue (Hunter)
+  Color(1.0, 0.0, 0.0),        # 3: Red (Bossman/Jazz)
+  Color(0.0, 1.0, 1.0),        # 4: Cyan (Iceman)
+  Color(1.0, 0.855, 0.725),    # 5: Peach (Angel)
+  Color(0.0, 0.0, 1.0),        # 6: Blue (Paladin/Doomsday)
+  Color(0.0, 1.0, 0.0),        # 7: Electric Green (Maniac)
+  Color(0.647, 0.165, 0.165),  # 8: Brown (Knight)
+  Color(1.0, 1.0, 0.0),        # 9: Yellow (Bluehair)
+  Color(0.259, 0.431, 0.024),  # 10: Pea Green (Shotglass)
+  Color(0.827, 0.827, 0.827),  # 11: medium light gray
+  Color(0.502, 0.502, 0.502),  # 12: medium gray
+  Color(0.961, 0.961, 0.961),  # 13: very light gray
+  Color(0.184, 0.184, 0.184),  # 14: very dark gray
+  Color(0.333, 0.333, 0.333),  # 15: medium dark gray
+  Color(0.827, 0.827, 0.827),  # 16: medium light gray
+]
 
+
+
+
+var is_playing: bool = true
+var has_ended: bool = false
 var current_dialog_index: int = 0
 var current_phoneme_index: int = -1
 var current_phoneme_duration: float = 0.0
 var current_phoneme_duration_left: float = 0.0
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+func set_dialogs(dialogs: Array[DialogProperties]) -> DialogPlayer:
+	self.mission_dialogs = dialogs
+	return self
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if mission_dialogs:
-		if current_dialog_index < len(mission_dialogs.bar_shotglass):
-			var current_dialog = mission_dialogs.bar_shotglass[current_dialog_index]
-			subtitles_label.text = current_dialog.text
+	if mission_dialogs and is_playing:
+		if current_dialog_index < len(mission_dialogs):
+			var current_dialog = mission_dialogs[current_dialog_index]
+			subtitles_label.text = _replace_placeholder(current_dialog.text)
+			subtitles_label.label_settings.font_color = subtitle_colors[current_dialog.text_color]
+			background = _get_scene("res://Assets/Cutscenes/Backgrounds/", current_dialog.background)
+			foreground = _get_scene("res://Assets/Cutscenes/Foregrounds/", current_dialog.foreground)
 			if current_phoneme_duration_left <= 0:
 				current_phoneme_index += 1
 				if current_phoneme_index < len(current_dialog.lip_sync_text):
 					var phoneme = current_dialog.lip_sync_text[current_phoneme_index]
 					if phoneme >= '0' and phoneme <= '9':
 						cutscene_character.mouth_index = 0
-						current_phoneme_duration = int(phoneme)
-						current_phoneme_duration_left = int(phoneme)
+						current_phoneme_duration = int(phoneme)/10
+						current_phoneme_duration_left = int(phoneme)/10
 					elif phoneme == "$":
 						cutscene_character.mouth_index = 0
-						current_phoneme_duration = current_dialog.delay
-						current_phoneme_duration_left = current_dialog.delay
 					else:
-						cutscene_character.mouth_index = phoneme_to_viseme_with_durations[phoneme]["viseme"]
-						current_phoneme_duration = phoneme_to_viseme_with_durations[phoneme]["duration"]
-						current_phoneme_duration_left = phoneme_to_viseme_with_durations[phoneme]["duration"]
+						var visime = phoneme_to_viseme_with_durations[phoneme]
+						cutscene_character.mouth_index = visime["viseme"] if visime else 0
+						current_phoneme_duration = visime["duration"] if visime else 1
+						current_phoneme_duration_left = visime["duration"] if visime else 1
 				else:
 					current_dialog_index += 1
 					current_phoneme_index = -1
 			else:
-				current_phoneme_duration_left -= current_phoneme_duration * delta
+				current_phoneme_duration_left -= delta
 		else:
+			has_ended = true
+			is_playing = false
 			cutscene_character.mouth_index = 0
+
+
+
+func _get_scene(path, id: int):
+	var code = str(id).pad_zeros(3) + "_"
+	var dir = DirAccess.open(path)
+	if dir:
+		dir.list_dir_begin()
+		var file_name = dir.get_next()
+		while file_name and not file_name.begins_with(code):
+			file_name = dir.get_next()
+		if file_name.begins_with(code):
+			return load(file_name)
+	return null
+
+
+func _replace_placeholder(text: String) -> String :
+	return text.replace("$A", "MEDAL TO BE AWARDED") \
+			.replace("$C", World.game_state.player.callsign) \
+			.replace("$D", str(World.game_state.year) + ", " + str(World.game_state.date)) \
+			.replace("$E", "PREVIOUS DATE (FOR MEDAL CEREMONY)") \
+			.replace("$K", "KILLS LAST MISSION") \
+			.replace("$L", "WINGMAN KILLS LAST MISSION") \
+			.replace("$N", World.game_state.player.pilot_name) \
+			.replace("$P", "NAME (NEVER USED)") \
+			.replace("$R", "RANK") \
+			.replace("$S", "SYSTEM") \
+			.replace("$T", "TIME OF NEXT MISSION")
+
+
+func _on_texture_button_pressed() -> void:
+	if has_ended:
+		print("Exit click")
+		get_tree().change_scene_to_file("res://Scenes/OnBoard/Bar.tscn")
+	else:
+		print("Next shot click")
+		current_dialog_index += 1
